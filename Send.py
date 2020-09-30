af69ba0726dc0eeefe6c7a5df29f6a8f9dd48f38
OverFitted <33264129+OverFitted@users.noreply.github.com>
2020-09-30 21:20:19 +0300

    Update Send.py

--- Send.py
+++ Send.py
@@ -1,6 +1,6 @@
 def send_message(authed, post):
     authed.method("messages.send", {"chat_id": 1,
-                                    "message": 'Вышел новый пост, не забудь поставить лайк!',
+                                    "message": 'New post',
                                     "random_id": 0,
                                     "attachment": 'wall{}_{}'.format(post['owner_id'], post['id'])})
 
